40d9f3148e7d89896eb68eacab27e4a6b8288fc8
chandramoulidornala <chandramouli.dornala@gmail.com>
2020-07-13 16:52:41 +0530

    intial commit

---
+++ project3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            Console.WriteLine("enter the values for num1 and num2");
+            num1 = Convert.ToInt16(Console.ReadLine());
+            num2 = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine($"The before swapped numbers are {num1} and {num2}");
+            num1 = num1 + num2;
+            num2 = num1 - num2;
+            num1 = num1 - num2;
+            Console.WriteLine($"The swapped numbers are {num1} and {num2}");
+
+
+
+        }
+    }
+}
